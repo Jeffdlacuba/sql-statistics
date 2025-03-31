31c7cbac8913ba1987ec06cf19dfa4f900a5d16d
jeffdlc <jeffersondonline@gmail.com>
2025-03-30 23:22:45 -0500

    update coding

--- average_ride_duration_using_sql.py
+++ average_ride_duration_using_sql.py
@@ -22,3 +22,27 @@ df_location_averae_trips.info()
 
 # identificar los 10 principales barrios en términos de finalización del recorrido
 df_location_averae_trips.head(10)
+
+# hacer gráficos: empresas de taxis y número de viajes, los 10 barrios principales por número de finalizaciones
+
+
+# Prueba la hipótesis:
+# "La duración promedio de los viajes desde el Loop hasta el Aeropuerto Internacional O'Hare cambia los sábados lluviosos".
+# Decide por tu cuenta dónde establecer el nivel de significación (alfa).
+
+# Explica:
+# cómo planteaste las hipótesis nula y alternativa
+# qué criterio usaste para probar las hipótesis y por qué
+
+
+# ¿Cómo será evaluado mi proyecto?
+# Esto es lo que buscará el revisor del proyecto al evaluar tu proyecto:
+
+# cómo recuperas los datos del sitio web
+# cómo creas slices de datos
+# cómo agrupas los datos
+# si usas correctamente los diversos métodos para combinar datos
+# cómo formulas las hipótesis
+# qué criterios utilizas para probar las hipótesis y por qué
+# a qué conclusiones llegas
+# si dejas comentarios en cada paso
